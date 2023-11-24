74cda6705d3c315148dd307ec4865dbb0bf3b91c
chloe-reg <chloes.reg@gmail.com>
2023-11-24 15:58:35 +0800

    update comment

--- LeetCode_001_TwoSum.py
+++ LeetCode_001_TwoSum.py
@@ -1,5 +1,8 @@
-# 暴力破解法
+# 給定一個整數陣列 nums 和一個目標值 target，
+# 請你在陣列中找出兩個數字的索引，使得它們的和等於目標值。
+# 你可以假設每個輸入都只會有一個解，而你不可以使用相同的元素兩次。
 class Solution:
+    # 暴力破解法
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
@@ -8,7 +11,9 @@ class Solution:
         return [0, 0]
 
 
-nums = [3, 3]
+nums = [2, 6, 4]
 target = 6
 
 print(Solution.twoSum(Solution, nums, target))
+
+print(enumerate(nums))
